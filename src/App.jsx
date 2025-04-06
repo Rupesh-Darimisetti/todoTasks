a303b4d5020dd5404eb033f5b163fb3f3c0694d1
Rupesh-Darimisetti <rupeshdarimisetti@gmail.com>
2025-04-06 17:17:43 +0530

    local storgae working

--- src/App.jsx
+++ src/App.jsx
@@ -7,47 +7,58 @@ function App() {
   const [tasks, setTasks] = useState([])
   const [task, setTask] = useState('');
   const [toggle, setToggle] = useState(false);
-  const toggleChange = () => {
-    setToggle(!tasks.toggle)
+
+  const toggleChange = (task) => {
+    setToggle(!task.isToggle)
   }
 
-  const addTasks = () => {
+  const addTasks = (event) => {
+    event.preventDefault()
     let todoItem = {
       id: uuid(),
       item: task,
-      isToggle: false
+      isToggle: toggle
     }
-    console.log(todoItem)
-    // tasks.push(todoItem)
-    setTasks([...tasks, todoItem])
+    tasks.push(todoItem)
+    setTasks(tasks)
+    setToggle(todoItem.isToggle)
     setTask('')
-    localStorage.setItem('todos', JSON.stringify([...tasks]))
+    localStorage.setItem('todos', JSON.stringify(tasks))
   };
-  const deleteTask = id => setTasks(tasks.filter(ele => id != ele.id));
+  const deleteTask = id => {
+    console.log(tasks)
+    console.log(id)
+    setTasks(tasks.filter(ele => id !== ele.id))
+    localStorage.removeItem('todos', tasks.id)
+    localStorage.getItem('todos')
+  };
 
   const onChangeTodo = (event) => {
     event.preventDefault()
-    setTask(event.target.value)
-    localStorage.setItem('todos', JSON.stringify(...tasks))
+    let value = event.target.value
+    setTask(value.trim().length > 0 && value)
+    localStorage.setItem('todos', JSON.stringify({ ...tasks, task }))
   }
   useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(tasks))
-    localStorage.getItem('todos')
+    const tasks = JSON.parse(localStorage.getItem('todos'))
+    // localStorage.setItem('todos', JSON.stringify(tasks))
     if (tasks) {
       setTasks(tasks)
     }
-  }, [tasks])
+  }, [])
   return (
     <>
       <h1>Task Tracker</h1>
-      <input type="text" value={task} onChange={onChangeTodo} placeholder='todo task' />
-      <button onClick={addTasks}>Add Task</button>
+      <form onSubmit={addTasks}>
+        <input type="text" value={task} onChange={onChangeTodo} placeholder='todo task' />
+        <button onClick={addTasks}>Add Task</button>
+      </form>
       {/* <TodoItems todoElements={tasks} onDeleteITem={deleteTask} /> */}
       {tasks.map(data => (
-        <div className='todo' key={data?.id}>
-          <input type='checkbox' value={data?.id} id="item" onToggle={toggleChange} />
-          <label className='todo:checked' htmlFor="item">{data?.item}</label>
-          <button onSubmit={() => deleteTask(data?.id)}>delete</button>
+        <div className={toggle ? 'todo:checked' : 'todo'} key={data?.id}>
+          <input type='checkbox' value={data?.id} id={data?.id} onToggle={() => toggleChange(item)} />
+          <label htmlFor={data?.id}>{data?.item}</label>
+          <button onClick={() => deleteTask(data.id)}>delete</button>
         </div>
       ))}
     </>
